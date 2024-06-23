c2cdbbb0a517ae46acd48d6dbf1239115c3e7780
Anshika Pandey <pandeyanshikalko3@gmail.com>
2024-06-23 16:49:05 +0530

    Add files via upload
    Declaring Variable!!

---
+++ Lecture4-DSA-CipherSchools.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // Declare 2 variables and then assign them values of 3,5 and then add them
+    int a;
+    int b;
+
+    a=3;
+    b=5;
+
+    int c;
+    c = a+b;
+
+    cout<<c;
+    cout<<" Sa+b "<<endl;
+
+    cout<<"a+b: "<<c<<endl; // combining the two couts in one line only
+
+}
