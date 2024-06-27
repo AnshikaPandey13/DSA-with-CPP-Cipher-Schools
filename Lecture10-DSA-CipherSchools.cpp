71bbe7d6f42a13859b95c820f39f97325f57a374
Anshika Pandey <pandeyanshikalko3@gmail.com>
2024-06-27 17:00:17 +0530

    loops pt 2

---
+++ Lecture10-DSA-CipherSchools.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //INITIALIZTATION
+    // int i=0;
+    // int j=0,k=0;
+    // // for(i=1;;i++){
+    // //      cout<<i<<" ";
+    // // }
+    // j=i++;
+    // k=++i;
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+    int i;
+    for(i=0;i<101;i++){  // called the update which happens after the iteration(part of a lopp is completed)
+        // block which gets executed everytime
+        cout<<i<<" ";
+    }
+
+}
