0e8851ddaf48800c048c5ebec7e635a35919f304
Anshika Pandey <pandeyanshikalko3@gmail.com>
2024-07-11 19:32:07 +0530

    commit the file

---
+++ Lecture20-DSA-CipherSchools.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long int b;
+    int a;
+    float c;
+    cout<<"The size of int is : "<<sizeof(a)<<" The size of long long int is: "<<sizeof(b)<<endl;
+    cout<<"The address of a is "<<&a<<" The address of b is "<<&b<<" The address of c is "<<&c;
+    return 0;
+} 
