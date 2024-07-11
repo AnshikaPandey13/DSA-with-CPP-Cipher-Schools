1c22a1fcb6ebd8d1fa218a41338200ddf1ea1b53
Anshika Pandey <pandeyanshikalko3@gmail.com>
2024-07-11 19:53:46 +0530

    commit the file

---
+++ Lecture21-DSA-CipherSchools.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    float b;
+    cout<<"The address of a is "<<&a<<endl;
+    cout<<"The address of b is "<<&b<<endl;
+    //whenever we have to define a pointer we have to assign it
+    int *p;
+    p=&a;
+
+}
