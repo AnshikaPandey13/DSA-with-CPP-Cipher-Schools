5a3a59ecaba4eeeeee462d622cf32c138e76b232
Anshika Pandey <pandeyanshikalko3@gmail.com>
2024-07-16 00:17:52 +0530

    commit the file

---
+++ Lecture27-DSA-CipherSchools.cpp
@@ -0,0 +1,29 @@
+//Lecture 27 - Character Array And Strings Pt 2
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string a;
+    string b;
+    string c;
+    // int a,b,c;
+
+
+    //Instead of using cin use something called getline
+    //cin>>a;
+    getline(cin,a);
+
+    //GETLINE is func. which is why you can see (), (cin,variable where you want to store the value of the line which is being taken as input)
+
+
+    //cin>>b;
+
+    //cout<<"The strings which you have given as input are "<<a<<" "<<b<<endl;
+   
+    //c=a+b;  //This is called string concatenation //Or in simple terms addition
+
+    // cout<<"The combined word is: "<<c<<endl;
+    // cout<<"The length of the combined name is: "<<c.length();
+
+        
+}
